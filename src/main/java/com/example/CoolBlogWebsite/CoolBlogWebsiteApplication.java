01040a4c1b51000ea31f26359598f992a3bfe363
MsHitai <mmalafeeva@yandex.ru>
2023-08-09 14:42:12 +0300

    initial commit

---
+++ src/main/java/com/example/CoolBlogWebsite/CoolBlogWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.CoolBlogWebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoolBlogWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoolBlogWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CoolBlogWebsite/CoolBlogWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CoolBlogWebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoolBlogWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
